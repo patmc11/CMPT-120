9a3a1fd93aab3dd4c6407a94542d2cd80ab1842f
Patrick McLaughlin <143512450+patmc11@users.noreply.github.com>
2023-09-17 19:50:02 -0400

    ff

--- Assignment 1/helloworld.py
+++ Assignment 1/helloworld.py
@@ -1,16 +1,10 @@
 #instructions: print out "Hello World!"
 #create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is: "
 #create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
+print("Hello World!")
 
+color = ("blue")
+print("My favorite color is",color)
 
-
-def main():
-
-
-
-    
-    
-    
-    
-    
-main()
+bday = "April 11th 2005"
+print("My birthday is", bday)
