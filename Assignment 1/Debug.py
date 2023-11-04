6995e4c8143891d91ed4f81e2c7cb11a0bf9554b
Patrick McLaughlin <143512450+patmc11@users.noreply.github.com>
2023-11-04 15:35:36 -0400

    dfg

--- Assignment 1/Debug.py
+++ Assignment 1/Debug.py
@@ -1,19 +1,5 @@
+Animals = ["dog" , "snake" , "bear"]
+Animals.pop(1)
+print(Animals)
 
-#Assignment 1: Debug.py
-#Fix any syntax errors you can spot and fulfill any other requests
-print("hello")
-print(3)
-print (10 + 10)
 
-print ("My name is Pat")
-print(50)
-print (2 + 2)
-
-name = ("Pat")
-print("Hello, my name is ", name)
-
-name2 = "Jack"
-print("Hello, my best friend's name is ", name2)
-
-age = 18
-print("I'm ", name, "and I'm " , age,"years old")
